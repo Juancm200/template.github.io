e8c401c6bc6349cb40c4929e05401770e9564643
Juan David López <35150736+juandld@users.noreply.github.com>
2022-08-05 15:32:36 -0500

    package update

--- src/routes/firebase.js
+++ src/routes/firebase.js
@@ -8,7 +8,13 @@ import {getAuth} from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    //api key
+    apiKey: "AIzaSyD13EAsUw-upXkKL13_aWA01HacUYzj-Mo",
+    authDomain: "product-template-e3477.firebaseapp.com",
+    projectId: "product-template-e3477",
+    storageBucket: "product-template-e3477.appspot.com",
+    messagingSenderId: "476481484767",
+    appId: "1:476481484767:web:7074bb1d624f717e3c766a",
+    measurementId: "G-FR58HL6NQ8"
 };
 
 // Initialize Firebase
